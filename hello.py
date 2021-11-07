fff800e97007051dc2557888d1130cae31d2c879
Kasia Siedlarek <kasia@siedlarek.net>
2021-11-07 13:26:35 +0100

    action

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 def print_name(name="World"):
     return f"Hello {name}\n"
 
+
 print(print_name())
--- test_hello.py
+++ test_hello.py
@@ -4,5 +4,6 @@ from hello import print_name
 def test_hello_default():
     assert print_name() == "Hello World\n"
 
+
 def test_hello_name():
     assert print_name("George") == "Hello George\n"
