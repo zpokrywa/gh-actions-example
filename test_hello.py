c449ba0f496260b9d09e64264489eda27214e443
Kasia Siedlarek <kasia@siedlarek.net>
2021-11-07 13:19:07 +0100

    action

---
+++ test_hello.py
@@ -0,0 +1,8 @@
+from hello import print_name
+
+
+def test_hello_default():
+    assert print_name() == "Hello World\n"
+
+def test_hello_name():
+    assert print_name("George") == "Hello George\n"
